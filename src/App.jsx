4fc1b0614ae28bd1c93cdf77fa849d9e2b0e87be
liviu-o <li.viu@mail.com>
2023-07-31 15:03:37 +0100

    Form buttons and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 //Important Setting up a react project using vite will require to install es lint npm install eslint vite-plugin-eslint eslint-config-react-app --save-dev
 //Important create a .eslintrc.json file and add export react-app then import into vite config and add to dependencie array
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Product from "../pages/Product";
 import Pricing from "../pages/Pricing";
@@ -11,6 +11,7 @@ import Login from "../pages/Login";
 import CityList from "../components/CityList";
 import CountryList from "../components/CountryList";
 import City from "../components/City";
+import Form from "../components/Form";
 
 const BASE_URL = "http://localhost:8000";
 
@@ -42,10 +43,7 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate replace to="cities" />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
@@ -55,7 +53,7 @@ function App() {
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
