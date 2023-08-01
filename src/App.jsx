64e68520a77178deeaf5dcd995dd356cbedeef3e
Liviu Cauia <Liviu.Cauia@hrp.org.uk>
2023-08-01 13:57:37 +0000

    Use custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 //Important Setting up a react project using vite will require to install es lint npm install eslint vite-plugin-eslint eslint-config-react-app --save-dev
 //Important create a .eslintrc.json file and add export react-app then import into vite config and add to dependencie array
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { useState, useEffect } from "react";
 import Product from "../pages/Product";
 import Pricing from "../pages/Pricing";
 import Homepage from "../pages/Homepage";
@@ -12,30 +11,13 @@ import CityList from "../components/CityList";
 import CountryList from "../components/CountryList";
 import City from "../components/City";
 import Form from "../components/Form";
+import { CitiesProvider, useCities } from "../contexts/CitiesContext";
 
-const BASE_URL = "http://localhost:8000";
+
 
 function App() {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(function () {
-    async function fetchCities() {
-      try {
-        setIsLoading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
-        const data = await res.json();
-        setCities(data);
-      } catch {
-        alert("There was an error loading data");
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    fetchCities();
-  }, []);
-
   return (
+    <CitiesProvider>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
@@ -46,18 +28,19 @@ function App() {
           <Route index element={<Navigate replace to="cities" />} />
           <Route
             path="cities"
-            element={<CityList cities={cities} isLoading={isLoading} />}
+            element={<CityList />}
           />
           <Route path="cities/:id" element={<City />} />
           <Route
             path="countries"
-            element={<CountryList cities={cities} isLoading={isLoading} />}
+            element={<CountryList  />}
           />
           <Route path="form" element={<Form />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
+    </CitiesProvider>
   );
 }
 
